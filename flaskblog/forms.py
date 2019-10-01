a75336463592f9e496d5669a21795fec1f865591
AlviEbneAlam <t88.alam@gmail.com>
2019-10-01 15:50:05 +0600

    paginating the blog posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -21,13 +21,13 @@ class RegistrationForm(FlaskForm):
 	def validate_field(self, email):
 		user=User.query.filter_by(email=email.data).first()
 		if user:
-			raise ValidationError('That email id already exists')		
+			raise ValidationError('That email id already exists')
+
 
 class UpdateAccountForm(FlaskForm):
 	username = StringField('Username', validators= [DataRequired(), Length(min=2, max=20)])
 	email= StringField('Email', validators= [DataRequired(), Email()])
 	picture=FileField('Update profile picture', validators=[FileAllowed(['jpg','png'])])
-
 	submit=SubmitField('Update')
 
 	def validate_username(self, username):
@@ -49,3 +49,9 @@ class LoginForm(FlaskForm):
 	remember= BooleanField('Remember me')
 
 	submit=SubmitField('Login')
+
+
+class PostForm(FlaskForm):
+	title=StringField('Title',validators=[DataRequired()])
+	content= TextAreaField('Content',validators=[DataRequired()])
+	submit=SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,33 +1,27 @@
 import secrets
 import os
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app,db,bcrypt 
 from flaskblog.models import User, Post
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    { 'author': 'Alvi Ebne Alam', 
-    'title':'Blog post 1',
-    'content':'First post content',
-    'date_posted':'6th September, 2019'
-    },
-     {'author': 'Tanzim Alam', 
-    'title':'Blog post 2',
-    'content':'Second post content',
-    'date_posted':'7th September, 2019'}]
-
-
+#route for home page
 @app.route('/')
 @app.route('/home')
 def home():
+    page=request.args.get('page',1, type=int)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
+
     return render_template('home.html', posts=posts)
 
+#route for about page
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')    
 
+#route for register page
 @app.route('/register', methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
@@ -42,6 +36,7 @@ def register():
         return redirect(url_for('login'))  
     return render_template('register.html', title='Register', form=form)
 
+#route for login page
 @app.route('/login', methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
@@ -58,12 +53,13 @@ def login():
 
     return render_template('login.html', title='Login', form=form)    
 
+#route for logout page
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-
+#function for saving uploaded picture
 def save_picture(form_picture):
     random_hex=secrets.token_hex(8)
     _, f_ext=os.path.splitext(form_picture.filename)
@@ -76,8 +72,7 @@ def save_picture(form_picture):
     return picture_fn 
 
 
-
-
+#route for account page
 @app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
@@ -97,3 +92,66 @@ def account():
 
     image_file=url_for('static',filename='profile_pics/'+current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form) 
+
+#route for new posts
+@app.route('/post/new',methods=['GET','POST'])
+@login_required
+def new_post():
+  form=PostForm()
+  if form.validate_on_submit():
+    post = Post(title=form.title.data, content=form.content.data, author=current_user)
+    db.session.add(post)
+    db.session.commit()
+    flash('Your post has been created')
+    return redirect(url_for('home'))
+  return render_template('create_post.html', title='New Post', form=form,legend='New Post')
+
+#route for individual posts
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post=Post.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title, post=post)
+
+#route for updating post
+@app.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form=PostForm()
+    if form.validate_on_submit():
+        post.title=form.title.data
+        post.content=form.content.data
+        db.session.commit()
+        flash('Your post has been updated!','success')
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method=='GET':
+        form.title.data=post.title
+        form.content.data=post.content
+
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+    #route for delete
+@app.route('/post/<int:post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post=Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been updated!','success')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/user/<string:username>')
+def user_post(username):
+    page=request.args.get('page',1, type=int)
+    user=User.query.filter_by(username=username).first_or_404()
+    posts=Post.query.filter_by(author=user)\
+    .order_by(Post.date_posted.desc())\
+    .paginate(page=page,per_page=5)
+
+    return render_template('user_post.html', posts=posts, user=user)
